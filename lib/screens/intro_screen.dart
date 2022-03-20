02fdd13227cd7061504d54cfecff9b126b9e2b50
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-03-20 23:42:42 +0100

    first commit

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  PageController _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _controller,
+            children: [
+              introPage('collaborate', 'Hire Top Talents',
+                  'Sign up to disconnect and hire for your next project'),
+              introPage('get_in_touch', 'Getting Started is Super Easy',
+                  'Instantly build your profile, learn and apply for your next job remotely'),
+              Container(color: Colors.yellow),
+              Container(color: Colors.green),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget introPage(String imgUrl, String title, String message) {
+    return Column(
+      children: [Image.asset('assets/image/$imgUrl.png')],
+    );
+  }
+}
