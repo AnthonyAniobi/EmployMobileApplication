a5ab8bf5aea63b10ab4e4d3d5e7a171d5a347cd1
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-03-20 23:58:16 +0100

    set orientation and fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:employ/screens/intro_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
 
