3b7b79e61550875d5ee6bcd93515fbdc16ceaaf0
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-03-28 17:43:36 +0100

    password onboarding page

--- lib/screens/sign_in/password_entry_page.dart
+++ lib/screens/sign_in/password_entry_page.dart
@@ -1,9 +1,20 @@
 import 'package:employ/constants/custom_colors.dart';
 import 'package:employ/constants/custom_fonts.dart';
+import 'package:employ/widgets/custom_buttons.dart';
+import 'package:employ/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
 class PasswordEntryPage extends StatelessWidget {
-  const PasswordEntryPage({Key? key}) : super(key: key);
+  PasswordEntryPage({Key? key}) : super(key: key);
+
+  final TextEditingController _password = TextEditingController();
+
+  void forgotPassword(BuildContext context) {}
+  void goBack(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  void login(BuildContext context) {}
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +23,54 @@ class PasswordEntryPage extends StatelessWidget {
       body: Column(
         children: [
           const SizedBox(height: 50),
-          CFont.primary('Welcome Onboard', color: CColor.black),
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    goBack(context);
+                  },
+                  icon: const Icon(Icons.arrow_back_ios)),
+            ],
+          ),
+          const Spacer(),
+          CFont.primary('Welcome Onboard!', color: CColor.black),
+          const SizedBox(height: 20),
           CFont.small('Please enter your password to continue',
               color: CColor.black),
-          CFont.small('Password', color: CColor.black),
+          const Spacer(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: CFont.secondary('Password'),
+              ),
+              CField.password(
+                controller: _password,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                      onTap: () {
+                        forgotPassword(context);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: CFont.small('Forgotten Password?',
+                            color: CColor.red),
+                      )),
+                ],
+              ),
+            ],
+          ),
+          const Spacer(),
+          CButton.primary(
+              text: 'Login',
+              onPressed: () {
+                login(context);
+              }),
+          const Spacer(),
         ],
       ),
     );
--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -42,10 +42,7 @@ class CField {
   }
 
   static Widget password(
-      {required String hint,
-      required IconData icon,
-      required TextEditingController controller,
-      Function(String)? onSubmit}) {
+      {required TextEditingController controller, Function(String)? onSubmit}) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20),
       padding: const EdgeInsets.all(5),
@@ -53,32 +50,23 @@ class CField {
         borderRadius: BorderRadius.circular(10),
         border: Border.all(width: 2, color: CColor.grey),
       ),
-      child: Row(
-        children: [
-          const SizedBox(width: 30),
-          Icon(icon),
-          const SizedBox(width: 10),
-          Expanded(
-            child: TextField(
-              controller: controller,
-              obscureText: true,
-              onSubmitted: (String entry) {
-                if (onSubmit != null) {
-                  onSubmit(entry);
-                }
-              },
-              style: const TextStyle(
-                color: CColor.black,
-                fontSize: 18,
-                fontFamily: 'Lato',
-              ),
-              decoration: InputDecoration(
-                  hintText: hint,
-                  border: InputBorder.none,
-                  suffixIcon: Icon(Icons.visibility)),
-            ),
-          ),
-        ],
+      child: TextField(
+        controller: controller,
+        obscureText: true,
+        onSubmitted: (String entry) {
+          if (onSubmit != null) {
+            onSubmit(entry);
+          }
+        },
+        style: const TextStyle(
+          color: CColor.black,
+          fontSize: 18,
+          fontFamily: 'Lato',
+        ),
+        decoration: const InputDecoration(
+            hintText: 'password',
+            border: InputBorder.none,
+            suffixIcon: Icon(Icons.visibility_off)),
       ),
     );
   }
