29f245637c9acd9c643fa8c0828b7e2e4d4ae7c7
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-04-01 21:10:25 +0100

    added appbar

--- lib/screens/employer/employer_home.dart
+++ lib/screens/employer/employer_home.dart
@@ -17,6 +17,41 @@ class _EmployerHomeState extends State<EmployerHome> {
           //   expandedHeight: 500,
           //   pinned: true,
           // ),
+          SliverAppBar(
+            expandedHeight: 500,
+            floating: true,
+            pinned: true,
+            elevation: 0,
+            actions: [
+              IconButton(
+                  onPressed: () async {
+                    // final _temp = await NakamaAdmin().getUserData();
+                    // print(_temp);
+                    // print(await NakamaAdmin().getUserData());
+                    // _scaffoldKey.currentState.openDrawer();
+                  },
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  ))
+            ],
+            automaticallyImplyLeading: false,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(30),
+              ),
+            ),
+            backgroundColor: Colors.red,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              centerTitle: true,
+              title: Text(
+                "Pokedex",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              // background: _buildCard(),
+            ),
+          )
         ],
       ),
     );
